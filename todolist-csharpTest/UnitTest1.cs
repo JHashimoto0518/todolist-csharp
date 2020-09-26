c2869750a5747348eaeca725da8d1d390e966ce1
Junichi Hashimoto <junichi_hashimoto@hotmail.co.jp>
2020-09-27 07:13:39 +0900

    create test project

---
+++ todolist-csharpTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace todolist_csharpTest {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1 () {
+            Assert.Fail();
+         }
+    }
+}
